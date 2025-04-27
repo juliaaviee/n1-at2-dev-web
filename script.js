195a4f72612e167f634ff3965eb2d60ea0470f5e
Vini <ovini.15515@gmail.com>
2025-04-27 11:10:03 -0300

    oop básico

---
+++ script.js
@@ -0,0 +1,10 @@
+class Pokemon{
+    constructor(nome, tipo) {
+        this.nome = nome
+        this.tipo = tipo
+    }
+}
+
+let pokemonAgua = new Pokemon('Squirtle', 'Água')
+let pokemonFogo = new Pokemon('Charmander', 'Fogo')
+let pokemonTerra = new Pokemon('Bubassauro', 'Terra')
